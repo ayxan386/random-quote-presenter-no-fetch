33da0787d250c7bffc912d43c6254765aa5072a0
ayxan386 <36412590+ayxan386@users.noreply.github.com>
2019-07-21 23:14:40 +0400

    Add files via upload

---
+++ randomQuotes.js
@@ -0,0 +1,59 @@
+const quoteList = [
+  {
+    quote:
+      "Together we can change the world, just one random act of kindness at a time.",
+    author: "Ron Hall"
+  },
+  {
+    quote:
+      "Don't be afraid. Be focused. Be determined. Be hopeful. Be empowered.",
+    author: "Michelle Obama"
+  },
+  {
+    quote:
+      "A man should have the aim and the determination to be honest and upright and sincere in all that he undertakes. If he adds persistency to this he can hardly help being successful",
+    author: "L. R. Ellert"
+  },
+  {
+    quote:
+      "Only one thing is ever guaranteed, that is that you will definitely not achieve the goal if you don't take the shot",
+    author: "Wayne Gretzky"
+  },
+  {
+    quote:
+      "Children really brighten up a household. They never turn the lights off.",
+    author: "Ralph Bus"
+  },
+  {
+    quote:
+      "No one would have crossed the ocean if he could have gotten off the ship in the storm.",
+    author: "Charles Kettering"
+  },
+  {
+    quote: "Appreciate those early influences and what they've done for you",
+    author: "Willie Davis"
+  },
+  {
+    quote:
+      "The true wealth of a nation lies not in it's gold or silver but in it's learning, wisdom and in the uprightness of its sons",
+    author: "Kahlil Gibran "
+  },
+  {
+    quote: "Make the decision, make another. Remake one past, you cannot",
+    author: "Yoda"
+  },
+  {
+    quote:
+      "I think beauty comes from actually knowing who you are. That's real beauty to me.",
+    author: "Elllen DeGeneres"
+  },
+  {
+    quote:
+      "Memories, pressed between the pages of my mind. Memories, sweetened through the ages just like wine",
+    author: "Elvis Presley"
+  },
+  {
+    quote: "If you want to see a rainbow you have to learn to see the rain.",
+    author: "Paulo Coelho"
+  }
+];
